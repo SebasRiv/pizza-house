823f298492af7046f4f5090efa941a3603de0a95
Sebastian Rivera <sebastian-010198@hotmail.com>
2020-08-15 21:32:59 -0500

    Web form

--- resources/views/pizzas/create.blade.php
+++ resources/views/pizzas/create.blade.php
@@ -1,13 +1,30 @@
 @extends('layouts.layout')
 
 @section('content')
-<div class="flex-center position-ref full-height">
+<div class="wrapper create-pizza">
+    <h1>Create a New Pizza</h1>
+    <form action="/pizzas" method="POST">
+        <label for="name">Your name:</label>
+        <input type="text" id="name" name="name">
 
-    <div class="content">
-        <div class="title m-b-md">
-            Add a New Pizza
-        </div>
+        <label for="type">Choose pizza type:</label>
+        <select name="type" id="type">
+            <option value="margarita">Margarita</option>
+            <option value="hawaiian">Hawaiian</option>
+            <option value="veg supreme">Veg supreme</option>
+            <option value="volcano">Volcano</option>
+        </select>
 
-    </div>
+        <label for="base">Choose base type:</label>
+        <select name="base" id="base">
+            <option value="cheesy crust">Cheesy Crust</option>
+            <option value="galic crust">Galic Crust</option>
+            <option value="thin & cripsy">Thin & Crispy</option>
+            <option value="thick">Thick</option>
+        </select>
+
+        <input type="submit" value="Order Pizza">
+    
+    </form>
 </div>
 @endsection
