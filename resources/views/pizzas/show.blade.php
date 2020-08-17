f07038b0584dafad74c5c19b39eaa737b3c417a9
Sebastian Rivera <sebastian-010198@hotmail.com>
2020-08-16 23:20:48 -0500

    Arrays & JSON

--- resources/views/pizzas/show.blade.php
+++ resources/views/pizzas/show.blade.php
@@ -4,7 +4,13 @@
 <div class="wraper pizza-details">
     <h1>Order for {{ $pizza->name }}</h1>
     <p class="type">Type is {{ $pizza->type }} </p>
-    <p class="base">Base {{ $pizza->base }}</p>
+    <p class="base">Base is {{ $pizza->base }}</p>
+    <p class="toppings">Extra toppings:</p>
+    <ul>
+        @foreach($pizza->toppings as $topping)
+        <li>{{ $topping }}</li>
+        @endforeach
+    </ul>
 </div>
 <a href="/pizzas" class="back"><- Back to all pizzas</a>
 @endsection
